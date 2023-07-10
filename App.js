e620f8fb2201401ccded82dfcc57d27e6896aa05
iamsreehari02 <sreeharivnambiar72@gmail.com>
2023-07-10 16:55:09 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import First from './components/header/headerComponent'
+
+
+
+function App() {
+  return (
+    <First></First>
+  
+  
+  );
+}
+
+export default App;
---
+++ components/header/headerComponent.js
@@ -0,0 +1,36 @@
+function Header(){
+    return(
+        <div className="header">
+            <div className="left_inside_header">
+                <div> <Image</div>
+                <div className="inferno"></div>
+            </div>
+            <div className="right_inside_header"></div>
+
+        </div>
+    )
+}
+export default Header
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
